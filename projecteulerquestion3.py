0e9fe5f6b2f1a6351bbbfe89c50b723b09825997
Comfort-Ekpe <eagbajicomfort@gmail.com>
2020-07-08 16:18:01 -0700

    restructured the shape.py code

--- projecteulerquestion3.py
+++ projecteulerquestion3.py
@@ -1,7 +1,51 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
-import shape
-print(shape.Circle(6.7))
-print(shape.Triangle(5,4))
-print(shape.Trapezoid(23,2,6))
+
+'''
+Project evaluation
+-> Take the value of the integer and store in a variable.
+-> Using a while loop, first obtain the factors of the number.
+-> Using another while loop within the previous one, compute if the factors are prime or not.
+-> Exit.
+
+'''
+v = []
+n = 600851475143
+i = 2
+while i * i < n:
+     while n % i == 0:
+         n = n / i
+     i = i + 1
+# print (n)
+# v.append(n)
+# v.sort()
+# print("The largest number among the factors of 600851475143 is:", v)
+
+
+
+# QUESTION FOUR
+'''
+The sum of the squares of the first ten natural numbers is,
+
+12+22+...+102=385
+The square of the sum of the first ten natural numbers is,
+
+(1+2+...+10)2=552=3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+'''
+import functools
+# from math import fsum
+sumn= []
+sqsum = []
+i = 1
+for i in range(2, 101):
+	v = i ** 2
+	sumn.append(v)	
+print("The sum of the squares of the first 100 natural numbers are", functools.reduce(lambda a,b : a + b, sumn))
+for i in range(2, 101):
+	sqsum.append(i)
+	print(sqsum ** 2)
+print(functools.reduce(operator.mul,))
--- shape.py
+++ shape.py
@@ -27,61 +27,97 @@ FUNCTIONS:
 		This finds the area of a Triangle using the formula A = 1/2b*h
 	-> Area Of Parallelogram
 		This finds the area of a Parallelogram using the formula A = bh
+	-> Area of Sector
+		This finds the area of a sector using the formula 1/2 r**2 * angle
 
 '''
 
-def rect(b, h):
+def findAreaOfRectangle(b, h):
 	'''
-	FUNCTION 	rect
-		@args: b, h -> Int or float
-		@returns: Int or float
+	FUNCTION NAME:	rect
+		This function computes the area of a rectangle which is the base times the  height
+		b = distance along the base of the rectangle
+		h = vertical height of the rectangle
+			@args: b, h -> Int or float
+			@returns: Int or float
 	'''
 	return b * h
 
-def circle(r, Pi = 3.142):
+def findAreaOfCircle(r, Pi = 3.142):
 	'''
-	FUNCTION 	circle
-		@args: r, Pi-> Int or float
-		@returns: Int or float
-	'''
+	FUNCTION NAME:	circle
+		This function computes the area of a circel which is Pi times the radius of the circle
+		r = radius of the circle measured in meters or centimeters
+		Pi = a constant which has the value f 22/7 or 3.142
+			@args: r, Pi-> Int or float
+			@returns: Int or float
+			'''
 	return Pi * (r**2)
 
-def square(s):
+def findAreaOfSquare(s):
 	'''
-	FUNCTION 	square
+	FUNCTION NAME:	square
+		This function computes the area of a square which is the square of the sides since all sides are equal
+		s = sides of the square
 		@args: s -> Int or float
 		@returns: Int or float
 	'''
 	return s ** 2
 
-def trapezoid(a, b, h):
+def findAreaOfTrapezoid(a, b, h):
 	'''
-	FUNCTION 	trapezoid
+	FUNCTION NAME: 	trapezoid
+h	This function computes the area of a trapezoid which is the average of the two base lenght times the altitude
+	a = upper base
+	b = lower base
+	h = height of the trapezoid
 		@args: a,b, h -> Int or float
 		@returns: Int or float
 	'''
 	return ((a + b)* h) / 2
 
-def ellipse(a, b, Pi = 3.142):
+def findAreaOfEllipse(a, b, Pi = 3.142):
 	'''
-	FUNCTION 	ellipse
+	FUNCTION NAME: 	ellipse
+	This function computes the area of an ellipse which is the bases times Pi
+	a = first base
+	b = second base
+	Pi = constant value 
 		@args: a,b, Pi -> Int or float
 		@returns: Int or float
 	'''
 	return Pi * a * b
 
-def triangle(b,h):
+def findAreaOfTriangle(b,h):
 	'''
-	FUNCTION 	triangle
-		@args: b, h -> Int or float
-		@returns: Int or float
+	FUNCTION NAME: 	triangle
+	This function computes the area of a triangle which is half of the base times the  height
+		b = distance along the base of the triangle
+		h = vertical height of the triangle measured at right angles to the base
+			@args: b, h -> Int or float
+			@returns: Int or float
 	'''
 	return 0.5 * b * h
 
-def parallelogram(b,h):
+def findAreaOfParallelogram(b,h):
 	'''
-	FUNCTION 	parallelogram
+	FUNCTION NAME: 	parallelogram
+	This function computes the area of a parallelogram which is the base times the  height
+		b = distance along the base of the parallelogram
+		h = vertical height of the parallelogram 
+			@args: b, h -> Int or float
+			@returns: Int or float
+	'''
+	return 0.5 * b * h	
+
+
+def findsectorOfACircle(v, r):
+	'''
+	FUNCTION NAME:	sector
+	This function computes the area of a sector which is the average of the angle in radian times the radius
 		@args: b, h -> Int or float
 		@returns: Int or float
 	'''
-	return 0.5 * b * h	
+	v = int(input("Enter the angle in radians of the sector: "))
+	return 0.5 * (r ** 2) * v
+
